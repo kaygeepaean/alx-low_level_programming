3bf5e6f5154c8449e94a790bbd58be179302bbf5
kaygeepaean <mehlapekgomotso@gmail.com>
2023-06-19 05:57:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*Description: 'alphabet in lowercase, and then in uppercase'
+*Return:always 0
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
+

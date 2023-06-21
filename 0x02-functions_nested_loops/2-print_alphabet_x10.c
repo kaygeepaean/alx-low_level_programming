32a9900ab6ac2d664b280ffa06ea039c8c290c60
kaygeepaean <mehlapekgomotso@gmail.com>
2023-06-21 11:54:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,17 @@
 *Description: 'print_alphabet_x10'
 *Return:always 0
 */
-void print_alphabet_x10(void
+void print_alphabet_x10(void)
 {
 int i;
 int j;
 	
-for (j=1; i <= 10; i++)
+for (i=1; i <= 10; i++)
 {	
-for (j = 97; j <= 122; i++)
+for (j = 97; j <= 122; j++)
 {
 _putchar(j);
 }
 _putchar('\n');
-}		
+}
 }

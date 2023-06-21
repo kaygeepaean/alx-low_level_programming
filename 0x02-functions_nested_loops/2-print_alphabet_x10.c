ccee2f880d8b7ec1194d52352c33f83403599960
kaygeepaean <mehlapekgomotso@gmail.com>
2023-06-21 15:10:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include"main.h"
 /**
-*main - Entry point
-*Description: 'void print_alphabet_x10'
-*Return:always 0
+*print_alphabet_x10-make alphabet_x10'
+*Return: void
 */
 void print_alphabet_x10(void)
 {

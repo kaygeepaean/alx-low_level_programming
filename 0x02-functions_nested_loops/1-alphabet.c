de910d399ce5aca071886f87e928b7ef4c625d53
kaygeepaean <mehlapekgomotso@gmail.com>
2023-06-21 07:44:20 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+*main - Entry point
+*Description: 'alphabet in lowercase, and then in uppercase'
+*Return:always 0
+*/
+void print_alphabet(void)
+{
+int i;
+	
+for (i = 97; i <= 122; i++)
+_putchar(i);
+}
+_putchar('\n')
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+*main - Entry point
+*Description: 'print_alphabet_x10'
+*Return:always 0
+*/
+void print_alphabet_x10(void
+{
+int i;
+int j;
+	
+for (j=1; i <= 10; i++)
+{	
+for (j = 97; j <= 122; i++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}		
+}

80e9a46d87dc59d9d4e7092730fb0fe08e0fb154
kaygeepaean <mehlapekgomotso@gmail.com>
2023-07-07 12:50:43 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* _puts - Prints a string followed by a new line
+* @str: The string to be printed
+*
+* Return: None
+*/
+void _puts(char *str);
+int main()
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
